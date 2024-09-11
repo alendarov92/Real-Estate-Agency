9ff26945f419fb6329e9d91a912ed0c2c59dbde5
alendarov92 <104994198+alendarov92@users.noreply.github.com>
2024-09-11 09:46:23 +0100

    Authentication done

--- config/env.js
+++ config/env.js
@@ -1,3 +1,4 @@
 exports.PORT = 3000; 
 exports.DB_QUERYSTRING = 'mongodb://localhost:27017/realEstateAgency';
 exports.SALT_ROUNDS = 10;
+exports.SECRET = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,42 +1,58 @@
 const router = require('express').Router();
 
 const authService = require('../services/authServices')
-
+const { COOKIE_SESSION_NAME } = require('../constants');
+const { isAuth, isGuest } = require('../middlewares/authMiddleware');
 
 // ========> LOGIN
 
-router.get('/login', (req, res) => {
+router.get('/login', isGuest, (req, res) => {
     res.render('auth/login')
+})
+
+router.post('/login', isGuest,  async (req, res) => {
+    const { username, password } = req.body;
+    const user = await authService.login(username, password);
+    const token = await authService.createToken(user)
+
+    res.cookie(COOKIE_SESSION_NAME, token, { httpOnly: true })
+
+    res.redirect('/')
 })
 
 
 // ========> REGISTER
-        // Get the page
-        router.get('/register', (req, res) => {
-            res.render('auth/register')
-        });
+// Get the page
+router.get('/register', isGuest,  (req, res) => {
+    res.render('auth/register')
+});
 
 
-        // Make the actions
-        router.post('/register', async (req, res) => {
-        
-            const { password, repeatePassword, ...userData } = req.body;
-        
-            // if (password !== repeatePassword) {
-            //     return res.render('auth/register', { error: 'Password dont mach!' })
-            // }
-        
-            try {
-                await authService.create({ password, ...userData });
-            
-                res.redirect('/')
-            
-            } catch (error) {
-                return res.render('auth/register', { error: 'db error' })
-            
-            }
-        
-        })
+// Make the actions
+router.post('/register', isGuest, async (req, res) => {
+
+    const { password, repeatePassword, ...userData } = req.body;
+
+    if (password !== repeatePassword) {
+        // Returnig err notfication for not matching passwords
+        return res.render('auth/register', { error: 'Passowrds not matching' })
+    }
+
+    try {
+        await authService.create({ password, ...userData });
+
+        res.redirect('/')
+
+    } catch (error) {
+        return res.render('auth/register', { error: 'db error' })
+
+    }
+
+})
 
 // ========> LOGOUT
+router.get('/logout', isAuth, (req, res) => {
+    res.clearCookie(COOKIE_SESSION_NAME)
+    res.redirect('/')
+})
 module.exports = router;
